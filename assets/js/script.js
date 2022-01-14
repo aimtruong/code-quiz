74d700d1e034df034d9510227d2700bec40e9649
Aimee Truong <aimtruong@ucdavis.edu>
2022-01-14 13:14:31 -0800

    pesudo-code written

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+
+
+
+
+// start function
+
+
+// timer function
+
+
+    // when timer > 0
+
+
+    // when timer is 0
+
+
+// show question
+
+
+
+// click eventlistener for answer
+
+
+    // when correct
+
+
+    // when wrong
+
+
+// game over function
+
+    // save initials and score in local storage
