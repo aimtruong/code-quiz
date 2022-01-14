e44db38cdd108a9aac5ac517dfd729e7238c686b
Aimee Truong <aimtruong@ucdavis.edu>
2022-01-14 15:50:01 -0800

    questions created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,43 @@
 // global variables
+var q1 = "What is an example of a property name?";
+var ans1 = [
+    "1. Color",
+    "2. Background-image",
+    "3. Font-family",
+    "4. All of the above"
+]
 
+var q12 = "You can center an image by doing display: block and then _________";
+var ans2 = [
+    "1. Padding: 10px",
+    "2. Flex: wrap",
+    "3. Margin: 0 auto",
+    "4. Margin: auto 0"
+]
+
+var q3 = "How can you create a new branch and move to that branch?";
+var ans3 = [
+    "1. Git checkout -b <branch-name>",
+    "2. Git checkout <branch-name>",
+    "3. Git-branch <branch-name>",
+    "4. Git checkout -d <branch-name>"
+]
+
+var q4 = "The condition in an if / else statement is enclosed with";
+var ans4 = [
+    "1. Parenthesis",
+    "2. Curly brackets",
+    "3. Semicolon",
+    "4. Square brackets"
+]
+
+var q5 = "Where can you NOT use a break statment in?";
+var ans5 = [
+    "1. A switch",
+    "2. A for loop",
+    "3. An if/else statement",
+    "4. A while loop"
+]
 
 
 // start function
@@ -15,7 +53,9 @@
 
 
 // show question
-
+function displayQuestion() {
+    
+}
 
 
 // click eventlistener for answer
@@ -30,3 +70,4 @@
 // game over function
 
     // save initials and score in local storage
+
