11b865da7c537c764d50b46bd90f1d44452963c0
Aimee Truong <aimtruong@ucdavis.edu>
2022-01-21 18:51:23 -0800

    high score list made, playerInfo input in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ var ans5El = document.createElement("form");
 
 // create element for questions and arrays
     // for question 01 // ans1AEl
-        var q1 = "What is an example of a event listener?";
+        var q1El = "What is an example of a event listener?";
 
         var ans1AEl = document.createElement("button");
             ans1AEl.textContent = "1. Submit";
@@ -94,26 +94,272 @@ ans4El.append(ans4AEl, ans4BEl, ans4CEl, ans4DEl);
 ans5El.append(ans5AEl, ans5BEl, ans5CEl, ans5DEl);
 
 
-// view high scores button
-var header = document.querySelector("#view-HS");
+// high score list
+var hs = document.createElement("form");
 
+// button to go back to main page
+var goBackBtn = document.createElement("button");
+    goBackBtn.textContent = "Go Back";
+    
+// button to clear high scores
+var clearBtn = document.createElement("button");
+    clearBtn.textContent = "Clear High Scores";
+
+// high scores
+var first = document.createElement("ol");
+var second = document.createElement("ol");
+var third = document.createElement("ol");
+var fourth = document.createElement("ol");
+var fifth = document.createElement("ol");
+
+var firstEl = {
+    name: "n/a",
+    highscore: 0
+}
+
+var secondEl = {
+    name: "n/a",
+    highscore: 0
+}
+
+var thirdEl = {
+    name: "n/a",
+    highscore: 0
+}
+
+var fourthEl = {
+    name: "n/a",
+    highscore: 0
+}
+
+var fifthEl = {
+    name: "n/a",
+    highscore: 0
+}
+    
+localStorage.setItem("first HS", JSON.stringify(firstEl));
+localStorage.setItem("second HS", JSON.stringify(secondEl));
+localStorage.setItem("third HS", JSON.stringify(thirdEl));
+localStorage.setItem("fourth HS", JSON.stringify(fourthEl));
+localStorage.setItem("fifth HS", JSON.stringify(fifthEl));
+
+var firstHS = localStorage.getItem("first HS");
+    firstHS = JSON.parse(firstHS);
+
+var secondHS = localStorage.getItem("second HS");
+    secondHS = JSON.parse(secondHS);
+
+var thirdHS = localStorage.getItem("third HS");
+    thirdHS = JSON.parse(thirdHS);
+    
+var fourthHS = localStorage.getItem("fourth HS");
+    fourthHS = JSON.parse(fourthHS);
+    
+var fifthHS = localStorage.getItem("fifth HS");
+    fifthHS = JSON.parse(fifthHS);
+    
+first.textContent = "1. " + firstHS.name + " - " + firstHS.highscore;
+second.textContent = "2. " + secondHS.name + " - " + secondHS.highscore;
+third.textContent = "3. " + thirdHS.name + " - " + thirdHS.highscore;
+fourth.textContent = "4. " + fourthHS.name + " - " + fourthHS.highscore;
+fifth.textContent = "5. " + fifthHS.name + " - " + fifthHS.highscore;
+
+
+// startbutton
+var startbtnEl = document.querySelector(".start-button");
+
+// function start()
+function start(){
+    paragraph.textContent = "";
+        body.removeChild(startbtnEl);
+        question.textContent = q1El;
+        body.appendChild(ans1El);
+        timerStarts();
+        // addeventlistener for answer click
+        var questionAnswerEl = document.querySelectorAll("button");
+
+        var correctOrWrongEl = document.createElement("p");
+            correctOrWrongEl.textContent = "";
+            correctOrWrongEl.setAttribute("style", "font-style: italic, color: rgba(155, 145, 145, 0.747)");
+            body.appendChild(correctOrWrongEl);
+
+        for(var i = 1; i <= 5; i++){
+            questionAnswerEl.forEach(button => {
+                questionAnswerEl.addEventListener("click", (e) => {
+                            if(question.textContent === q1El){
+                                if(button == ans1AEl){
+                                question.textContent = q2El;
+                                body.appendChild(ans2El);
+                                correctOrWrongEl.textContent = "Correct!";
+                                }
+                                else{
+                                timeLeft = timeLeft - 10;
+                                question.textContent = q2El;
+                                body.appendChild(ans2El);
+                                correctOrWrongEl.textContent = "Wrong!";
+                                }
+                            }
+                            else if(question.textContent === q2El){
+                                if(button == ans2BEl){
+                                    question.textContent = q3El;
+                                    body.appendChild(ans3El);
+                                    correctOrWrongEl.textContent = "Correct!";
+                                }
+                                else{
+                                    timeLeft = timeLeft - 10;
+                                    question.textContent = q3El;
+                                    body.appendChild(ans3El);
+                                    correctOrWrongEl.textContent = "Wrong!";
+                                }
+                            }  
+                            else if(question.textContent === q3El){
+                                if(button == ans3AEl){
+                                    question.textContent = q4El;
+                                    body.appendChild(ans4El);
+                                    correctOrWrongEl.textContent = "Correct!";
+                                }
+                                else{
+                                    timeLeft = timeLeft - 10;
+                                    question.textContent = q4El;
+                                    body.appendChild(ans4El);
+                                    correctOrWrongEl.textContent = "Wrong!";
+                                }
+                            }
+                            else if(question.textContent === q4El){
+                                if(button == ans4AEl){
+                                    question.textContent = q5El;
+                                    body.appendChild(ans5El);
+                                    correctOrWrongEl.textContent = "Correct!";
+                                }
+                                else{
+                                    timeLeft = timeLeft - 10;
+                                    question.textContent = q5El;
+                                    body.appendChild(ans5El);
+                                    correctOrWrongEl.textContent = "Wrong!";
+                                }
+                            }  
+                            else if(question.textContent === q5El){
+                                if(button == ans5CEl){
+                                    body.removeChild(ans5El);
+                                    correctOrWrongEl.textContent = "Correct!";
+                                    savePlayerScore();
+                                    highScorePage();
+                                }
+                                else{
+                                    timeLeft = timeLeft - 10;
+                                    body.removeChild(ans5El);
+                                    correctOrWrongEl.textContent = "Wrong!";
+                                    savePlayerScore();
+                                    highScorePage();
+                                }
+                            }    
+                });
+            });
+        }
+}
+
+
+// go back button function
+function goBack(){
+    question.textContent = "Coding Quiz Challenge";
+    paragraph.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
+    body.appendChild(startbtnEl);
+    hs.textContent = "";
+    goBackBtn.disabled;
+    clearBtn.disabled;
+    startbtnEl.addEventListener("click", start);
+};
+
+// clear function 
+function clear(){
+    firstEl = {
+        name: "n/a",
+        highscore: 0
+    }
+
+    secondEl = {
+        name: "n/a",
+        highscore: 0
+    }
+
+    thirdEl = {
+        name: "n/a",
+        highscore: 0
+    }
+
+    fourthEl = {
+        name: "n/a",
+        highscore: 0
+    }
+
+    fifthEl = {
+        name: "n/a",
+        highscore: 0
+    }
+    
+    localStorage.setItem("first HS", JSON.stringify(firstEl));
+    localStorage.setItem("second HS", JSON.stringify(secondEl));
+    localStorage.setItem("third HS", JSON.stringify(thirdEl));
+    localStorage.setItem("fourth HS", JSON.stringify(fourthEl));
+    localStorage.setItem("fifth HS", JSON.stringify(fifthEl));
+
+    var firstHS = localStorage.getItem("first HS");
+    firstHS = JSON.parse(firstHS);
+
+    var secondHS = localStorage.getItem("second HS");
+    secondHS = JSON.parse(secondHS);
+
+    var thirdHS = localStorage.getItem("third HS");
+    thirdHS = JSON.parse(thirdHS);
+    
+    var fourthHS = localStorage.getItem("fourth HS");
+    fourthHS = JSON.parse(fourthHS);
+    
+    var fifthHS = localStorage.getItem("fifth HS");
+    fifthHS = JSON.parse(fifthHS);
+    
+    first.textContent = "1. " + firstHS.name + " " + firstHS.highscore;
+    second.textContent = "2. " + secondHS.name + " " + secondHS.highscore;
+    third.textContent = "3. " + thirdHS.name + " " + thirdHS.highscore;
+    fourth.textContent = "4. " + fourthHS.name + " " + fourthHS.highscore;
+    fifth.textContent = "5. " + fifthHS.name + " " + fifthHS.highscore;
+}
+
+// save player info
+var playerName = document.createElement("input[name='playerName']").value;
+var playerHS = document.querySelector("timeLeft");
+
+var playerInfo = {
+    name: playerName,
+    highscore: playerHS
+};
+
+
+localStorage.setItem(playerInfo);
+
+
+// view high scores button
+var highscorebtn = document.querySelector("#view-HS");
+    highscorebtn.addEventListener("click", highScorePage);
 
 // high score page
 function highScorePage(){
     question.textContent = "High Scores";
+    paragraph.textContent = "";
+    body.removeChild(startbtnEl);
+    highscorebtn.textContent = "";
 
-    var hs = document.querySelector(".HS-list");
+    body.appendChild(hs);
+    hs.append(first, second, third, fourth, fifth);
 
-first = document.createElement("ol");
-second = document.createElement("ol");
-third = document.createElement("ol");
-fourth = document.createElement("ol");
-fifth = document.createElement("ol");
+    body.append(goBackBtn, clearBtn);
+
+    goBackBtn.addEventListener("click", goBack);
+    clearBtn.addEventListener("click", clear);
 };
-
+    
 
 // timer
-var headerEl = document.querySelector("header");
 var timerEl = document.querySelector("#timer");
 var timeLeft = 60;
 
@@ -126,108 +372,17 @@ function timerStarts(){
           else{
             clearInterval(timeInterval);
             timerEl.textContent = "";
+            ans1El.textContent = "";
+            ans2El.textContent = "";
+            ans3El.textContent = "";
+            ans4El.textContent = "";
+            ans5El.textContent = "";
             highScorePage();
-          }
-        }, 1000);
+        }
+    }, 1000);
 };
 
 
-// create start button and eventlistener
-    var startbtnEl = document.querySelector(".start-button"); 
-    
-    startbtnEl.addEventListener("click", function(){
-        paragraph.textContent = "";
-        body.removeChild(startbtnEl);
-        question.textContent = q1;
-        body.appendChild(ans1El);
-        timerStarts();
-
-        // addeventlistener for answer click
-        var questionAnswerEl = document.querySelectorAll("button");
-
-        var correctOrWrongEl = document.createElement("p");
-            correctOrWrongEl.textContent = "";
-            correctOrWrongEl.setAttribute("style", "font-style: italic, color: rgba(155, 145, 145, 0.747)");
-            body.appendChild(correctOrWrongEl);
-
-        questionAnswerEl.forEach(button => {
-            button.addEventListener("click", (e) =>{
-                switch(question){
-                    case q1El:
-                        if(ans1AEl){
-                            question.textContent = q2;
-                            body.appendChild(ans2El);
-                            correctOrWrongEl.textContent = "Correct!";
-                            break;
-                        }
-                        else{
-                            timeLeft = timeLeft - 10;
-                            question.textContent = q2;
-                            body.appendChild(ans2El);
-                            correctOrWrongEl.textContent = "Wrong!";
-                            break;
-                        }
-                    case q2El:
-                        if(ans2BEl){
-                            question.textContent = q3;
-                            body.appendChild(ans3El);
-                            correctOrWrongEl.textContent = "Correct!";
-                            break;
-                        }
-                        else{
-                            timeLeft = timeLeft - 10;
-                            question.textContent = q3;
-                            body.appendChild(ans3El);
-                            correctOrWrongEl.textContent = "Wrong!";
-                            break;
-                        }
-                    case q3El:
-                        if(ans3AEl){
-                            question.textContent = q4;
-                            body.appendChild(ans4El);
-                            correctOrWrongEl.textContent = "Correct!";
-                            break;
-                        }
-                        else{
-                            timeLeft = timeLeft - 10;
-                            question.textContent = q4;
-                            body.appendChild(ans4El);
-                            correctOrWrongEl.textContent = "Wrong!";
-                            break;
-                        }
-                    case q4El:
-                        if(ans4AEl){
-                            question.textContent = q5;
-                            body.appendChild(ans5El);
-                            correctOrWrongEl.textContent = "Correct!";
-                            break;
-                        }
-                        else{
-                            timeLeft = timeLeft - 10;
-                            question.textContent = q5;
-                            body.appendChild(ans5El);
-                            correctOrWrongEl.textContent = "Wrong!";
-                            break;
-                        }
-                    case q5El:
-                        if(ans5CEl){
-                            body.removeChild(ans5El);
-                            highScorePage();
-                            correctOrWrongEl.textContent = "Correct!";
-                            break;
-                        }
-                        else{
-                            timeLeft = timeLeft - 10;
-                            body.removeChild(ans5El);
-                            highScorePage();
-                            correctOrWrongEl.textContent = "Wrong!";
-                            break;
-                        }
-                }
-            });
-        });
-    
-    });
-
-
-         
+// create start button and eventlistener    
+startbtnEl.addEventListener("click", start);
+        
